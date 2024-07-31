532a3e5b4ad582a2ad40070b20c37547d98f057e
Dhruv312 <valanddhruv0312@gmail.com>
2024-07-31 11:12:00 +0530

    abc

--- OOPS_PRACTICE/Interface/Task2.java
+++
@@ -1,9 +0,0 @@
-// Scenario 2: Messaging Service Integration
-// Develop an interface called "MessagingService" with methods like "sendMessage" and "receiveMessage." Implement classes like "EmailService" and "SMSService" that provide specific implementations for these interface methods.
-
-
-public class Task2 {
-   public static void main(String[] args) {
-    
-   } 
-}
